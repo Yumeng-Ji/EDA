807d03d6bf958cc8bf188225f29ef4d4992f0254
Samira034 <samira.tamang247@gmail.com>
2025-10-04 18:57:09 +0200

    Update Autor.java

--- Autor.java
+++ Autor.java
@@ -4,12 +4,12 @@ import java.io.*;
 public class Autor {
     private String id;
     private String nombre;
-    private HashSet<Publicacion> listaPublicaciones;
+    private HashSet<String> listaIdPublicaciones;
 
     public Autor(String pId, String pNombre) {
         this.id = pId;
         this.nombre = pNombre;
-        this.listaPublicaciones = new HashSet<Publicacion>();
+        this.listaIdPublicaciones = new HashSet<String>();
     }
 
     public String getId() {
@@ -20,12 +20,16 @@ public class Autor {
         return nombre;
     }
 
-    public HashSet<Publicacion> getListaPublicaciones() {
-        return listaPublicaciones;
+    public HashSet<String> getListaIdPublicaciones() {
+        return listaIdPublicaciones;
     }
 
-    public void addPublicacion(Publicacion publicacion) {
-        listaPublicaciones.add(publicacion);
+    public void añadirPublicacion(String pId) {
+        this.listaIdPublicaciones.add(pId);
+    }
+    public void eliminarPublicacion(String pId){
+        this.listaIdPublicaciones.remove(pId);
     }
 
 }
+
