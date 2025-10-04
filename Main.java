d8e669b47ba63412735b0d0f1cfbed6483e2c89f
Samira034 <samira.tamang247@gmail.com>
2025-10-04 19:24:35 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -8,6 +8,21 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) throws IOException, FileNotFoundException {
        /* Repositorio.getRepositorio().cargarAutor("C:\\Users\\Yu Meng Ji\\IdeaProjects\\EDA\\src\\Datuak\\authors-name-all.txt");
+       ArrayList<String> titulos = new ArrayList<>();
+            titulos.add("Ade");
+            titulos.add("Acbd");
+            titulos.add("Bbcd");
+            titulos.add("Cbcd");
+            titulos.add("Dcd");
+            System.out.println("Original titles:");
+            for (int i = 0; i < titulos.size(); i++) {
+                System.out.println((i+1) + ". " + titulos.get(i));
+            }
+            String[] ordenas = miRep.publicacionesOrdenadas(titulos);
+            System.out.println("\nSorted titles:");
+            for (int i = 0; i < ordenas.length; i++) {
+                System.out.println((i+1) + ". " + ordenas[i]);
+            }
         int tamanio =Repositorio.getRepositorio().tamañoHash();
         System.out.println(tamanio); */
 
